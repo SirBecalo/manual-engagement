5f2dbfb9a7232f9a2511de548b9e87c905550ee3
SirBecalo <41799258+SirBecalo@users.noreply.github.com>
2024-02-04 19:17:35 +0200

    Update All_engagment_graphs.py

--- All_engagment_graphs.py
+++ All_engagment_graphs.py
@@ -147,7 +147,6 @@ def visualize_combined_engagement(en_week_folders, ar_week_folders, img_name):
     ax.set_title("Weekly Email Open Frequencies (EN & AR)")
     plt.tight_layout()
     plt.savefig(img_name)
-    plt.show()
     # The function is now updated with a distinct color scheme for English and Arabic data.
 
 
